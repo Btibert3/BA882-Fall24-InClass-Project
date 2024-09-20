0764b395d9c30d78588867dc3831a4e56f03eb33
“Brock” <btibert@bu.edu>
2024-09-20 20:42:26 +0000

    start md schema, not yet done

---
+++ functions/md-schema/main.py
@@ -0,0 +1,45 @@
+import functions_framework
+
+@functions_framework.http
+def main(request):
+    
+    # instantiate the services 
+    sm = secretmanager.SecretManagerServiceClient()
+
+    # settings
+    project_id = 'btibert-ba882-fall24'
+    secret_id = 'mother_duck'   #<---------- this is the name of the secret your created above!
+    version_id = 'latest'
+
+    # Build the resource name of the secret version
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+
+    # Access the secret version
+    response = sm.access_secret_version(request={"name": name})
+    md_token = response.payload.data.decode("UTF-8")
+
+    # initiate the MotherDuck connection through an access token through
+    md = duckdb.connect(f'md:?motherduck_token={md_token}') 
+
+    ##################################################### create the schema
+
+    # some setup that we will use for this task (db and schema names)
+    db = 'sandbox'
+    schema = "dev"
+    db_schema = f"{db}.{schema}"
+
+    # define the DDL statement with an f string
+    create_db_sql = f"CREATE DATABASE IF NOT EXISTS {db};"   
+
+    # execute the command to create the database
+    md.sql(create_db_sql)
+
+    # confirm it exists
+    print(md.sql("SHOW DATABASES").show())
+
+    # create the schema
+    md.sql(f"CREATE SCHEMA IF NOT EXISTS {db_schema};") 
+
+    ##################################################### create the core tables
+
+    
