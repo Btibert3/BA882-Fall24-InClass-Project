3a90be1a8ee71459797dfdf88cb3a4e3b7690050
“Brock” <btibert@bu.edu>
2024-09-21 17:47:03 +0000

    add extract rss

--- prefect/deploy-functions.sh
+++ prefect/deploy-functions.sh
@@ -7,12 +7,33 @@
 gcloud config set project btibert-ba882-fall24
 
 # schema setup
+echo "======================================================"
+echo "deploying the schema setup"
+echo "======================================================"
+
 gcloud functions deploy dev-schema-setup \
     --gen2 \
     --runtime python311 \
     --trigger-http \
     --entry-point task \
     --source ./functions/schema-setup \
+    --stage-bucket btibert-ba882-fall24-functions \
+    --service-account etl-pipeline@btibert-ba882-fall24.iam.gserviceaccount.com \
+    --region us-central1 \
+    --allow-unauthenticated \
+    --memory 256MB 
+
+# extract rss
+echo "======================================================"
+echo "deploying the rss extractor"
+echo "======================================================"
+
+gcloud functions deploy dev-extract-rss \
+    --gen2 \
+    --runtime python311 \
+    --trigger-http \
+    --entry-point task \
+    --source ./functions/extract-rss \
     --stage-bucket btibert-ba882-fall24-functions \
     --service-account etl-pipeline@btibert-ba882-fall24.iam.gserviceaccount.com \
     --region us-central1 \
---
+++ prefect/functions/extract-rss/main.py
@@ -0,0 +1,90 @@
+# imports
+import functions_framework
+from google.cloud import secretmanager
+from google.cloud import storage
+import requests
+import feedparser
+import json
+import datetime
+import uuid
+
+# settings
+project_id = 'btibert-ba882-fall24'
+secret_id = 'mother_duck'   #<---------- this is the name of the secret you created
+version_id = 'latest'
+bucket_name = 'btibert-ba882-fall24-awsblogs'
+job_id = datetime.datetime.now().strftime("%Y%m%d%H%M") + "-" + str(uuid.uuid4())
+
+####################################################### helpers
+
+def upload_to_gcs(bucket_name, job_id, data):
+    """Uploads data to a Google Cloud Storage bucket."""
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+    blob_name = f"jobs/{job_id}/extracted_entries.json"
+    blob = bucket.blob(blob_name)
+
+    # Upload the data (here it's a serialized string)
+    blob.upload_from_string(data)
+    print(f"File {blob_name} uploaded to {bucket_name}.")
+
+    return f"gs://{bucket_name}/{blob_name}"
+
+
+####################################################### core task
+
+@functions_framework.http
+def task(request):
+
+    # instantiate the services 
+    sm = secretmanager.SecretManagerServiceClient()
+    storage_client = storage.Client()
+
+    # Build the resource name of the secret version
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+
+    # Access the secret version
+    response = sm.access_secret_version(request={"name": name})
+    md_token = response.payload.data.decode("UTF-8")
+
+    ####################################### get the feeds
+
+    feeds = [
+        'https://aws.amazon.com/blogs/big-data/feed/',
+        'https://aws.amazon.com/blogs/compute/feed/',
+        'https://aws.amazon.com/blogs/database/feed/',
+        'https://aws.amazon.com/blogs/machine-learning/feed/',
+        'https://aws.amazon.com/blogs/containers/feed/',
+        'https://aws.amazon.com/blogs/infrastructure-and-automation/feed/',
+        'https://aws.amazon.com/blogs/aws/feed/',
+        'https://aws.amazon.com/blogs/business-intelligence/feed/',
+        'https://aws.amazon.com/blogs/storage/feed/'
+    ]
+
+    feed_list = []
+    for feed in feeds:
+        try:
+            r = requests.get(feed)
+            feed = feedparser.parse(r.content)
+            print(feed.feed.title)
+            feed_list.append(feed)
+        except Exception as e:
+            print(e)
+            raise
+
+    print(f"The length of the feed list is {len(feed_list)}")
+
+
+    # flatten
+    entries = []
+    for feed in feed_list:
+        entries.extend(feed.entries)
+    print(f"There are {len(entries)} post entries")
+
+    # to a json string
+    entries_json = json.dumps(entries)
+
+    # write to gcs
+    gcs_path = upload_to_gcs(bucket_name, job_id, entries_json)
+
+    return {'gcs_path':gcs_path, 'num_entries': len(entries)}, 200
