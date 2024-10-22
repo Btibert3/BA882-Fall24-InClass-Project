19c5ce7b88090eb13b6412d0638d10e1497034db
Brock <btibert3@gmail.com>
2024-10-22 19:19:26 +0000

    parse out instances

--- ml/poc2/model/main.py
+++ ml/poc2/model/main.py
@@ -10,8 +10,9 @@ app = FastAPI()
 async def health_check():
     return {"status": "ok"}
 
-
+# TODO: list of dictionaries with text:body as input, and list of dicts ()
 @app.post("/predict")
 def predict_proba(texts: List[str]):
+    texts = texts['instances']
     proba = model.predict_proba(texts)
     return {"predictions": proba.tolist()}
