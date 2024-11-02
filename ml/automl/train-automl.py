fa3c83742ba7d7ebd6a2c2f7d4209d8d73a3c8ac
Brock <btibert3@gmail.com>
2024-11-02 14:55:34 +0000

    more updates

---
+++ ml/automl/train-automl.py
@@ -0,0 +1,40 @@
+from google.cloud import aiplatform
+
+aiplatform.init(
+    project='btibert-ba882-fall24',
+    location='us-central1',  
+)
+
+# Define your dataset parameters
+dataset = aiplatform.TabularDataset.create(
+    display_name="nhl-shots-2024",
+    gcs_source="gs://btibert-ba882-fall24-vertex-models/training-data/automl/shots_2024.csv"  # Path to your data in GCS
+)
+
+# Create an AutoML training job
+job = aiplatform.AutoMLTabularTrainingJob(
+    display_name="automl-shots-traing",
+    optimization_prediction_type="classification", 
+    optimization_objective="minimize-log-loss"  
+)
+
+# Kick off the training
+model = job.run(
+    dataset=dataset,
+    target_column="goal",  # Column that you want to predict
+    training_fraction_split=0.8,
+    validation_fraction_split=0.1,
+    test_fraction_split=0.1,
+    budget_milli_node_hours=1000,  # 1000 milli node hours = 1 node hour
+    model_display_name="automl-shots-model"
+)
+
+# deploy the endpoint
+endpoint = model.deploy(
+    machine_type="n2-standard-2",
+    min_replica_count=1,
+    max_replica_count=1
+)
+
+# tear down the endpoint
+endpoint.delete()
--- ml/custom-model/model/deploy-model.sh
+++ ml/custom-model/model/deploy-model.sh
@@ -31,3 +31,38 @@ gcloud ai models upload \
   --container-image-uri=gcr.io/btibert-ba882-fall24/blog-post-length-model \
   --container-predict-route=/predict \
   --container-health-route=/health
+
+echo "======================================================"
+echo "deploy the endpoint"
+echo "======================================================"
+
+# gcloud ai endpoints create \
+#   --region=us-central1 \
+#   --display-name=post-length-endpoint
+
+# mangually adding in the values for the endpoint first, and then the model
+
+gcloud ai endpoints deploy-model numeric_model_id \
+  --region=us-central1 \
+  --model=3491069264956227584 \
+  --display-name=post-length-deployment \
+  --machine-type=n1-standard-4 \
+  --min-replica-count=1 \
+  --max-replica-count=1 \
+  --service-account=vertex-ai-sa@btibert-ba882-fall24.iam.gserviceaccount.com \
+  --traffic-split=0=100
+
+
+# find the deployed model id
+# gcloud ai endpoints describe 5499828630391357440 \
+#     --region=us-central1
+
+# undeploy the model - id grabbed from above
+# gcloud ai endpoints undeploy-model 5499828630391357440 \
+#     --region=us-central1 \
+#     --deployed-model-id=4523928497856446464
+
+
+# tear down the endpoint
+# gcloud ai endpoints delete 5499828630391357440 \
+#     --region=us-central1 --quiet
