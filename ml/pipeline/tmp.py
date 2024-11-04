b62cf1f56e2d7e20e354cf3ff0178c6b600298d4
Brock <btibert3@gmail.com>
2024-11-04 16:05:19 +0000

    reorg the pipeline lab, tweak for rolling dataset

--- ml/pipeline/tmp.py
+++
@@ -1,5 +0,0 @@
-# Prefect training pipeline - experiments done first
-# cloud function to train and write artifact to gcs
-# register training run in cloud warehouse
-# compare the model, if not existant or better, replace artifacts
-# inference batch/real time is cloud function because fast, quick to iterate
--- ml/pipeline/trainer/dataset.sql
+++
@@ -1,5 +0,0 @@
-select
-    word_count
-    ,title
-from
-    awsblogs.ml.post_length
