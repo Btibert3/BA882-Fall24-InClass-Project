cba8436905da1809e8ac6ba7a363fb4753b75cbe
Brock <btibert@bu.edu>
2024-11-20 15:07:41 +0000

    updates, working poc, learnings

--- genai/controlfow/gcp-flow1.py
+++ genai/controlfow/gcp-flow1.py
@@ -3,17 +3,19 @@
 # pip install prefect-gcp
 # pip install "google-cloud-aiplatform>=1.64"
 
-from prefect_gcp import GcpCredentials
-gcp_credentials_block = GcpCredentials.load("ba882-fall24-etl-sa")
+########
 
-from google.oauth2.service_account import Credentials
-import json
+# from prefect_gcp import GcpCredentials
+# gcp_credentials_block = GcpCredentials.load("ba882-fall24-etl-sa")
 
-service_account_json_str = gcp_credentials_block.service_account_info.get_secret_value()
-credentials = Credentials.from_service_account_info(service_account_json_str)
+# from google.oauth2.service_account import Credentials
+# import json
 
-import controlflow as cf
-from langchain_google_vertexai import ChatVertexAI
+# service_account_json_str = gcp_credentials_block.service_account_info.get_secret_value()
+# credentials = Credentials.from_service_account_info(service_account_json_str)
+
+# import controlflow as cf
+# from langchain_google_vertexai import ChatVertexAI
 
 ########### ^^ ignore
 
@@ -31,26 +33,43 @@ from langchain_google_vertexai import ChatVertexAI
 model = ChatVertexAI(model="gemini-1.5-flash", credentials=credentials)
 
 import controlflow as cf # info warning, default is open ai is aggressive
-agent = cf.Agent(model=model)
+# agent = cf.Agent(model=model)
+
+cf.defaults.model = model
 
 classifier = cf.Agent(
     name="yes or no",
-    model = model,
+    # model = model,
     instructions="You have a single task.  reply simply with yes or no.  Your response is a single word, yes, or no.",
 )
 
 classifications = cf.run(
-    'Flip a coin',
+    'Is this sky blue',
     result_type=['yes', 'no'],
     agents=[classifier],
 )
 
+classifications
 
-# project_id = "btibert-ba882-fall24"   
-# region_id = "us-central1"
-# vertexai.init(project=project_id, location=region_id, credentials=credentials)
+emails = [
+    "Hello, I need an update on the project status.",
+    "Subject: Exclusive offer just for you!",
+    "Urgent: Project deadline moved up by one week.",
+]
 
-# # model = GenerativeModel("gemini-1.5-pro-001")
+
+reply = cf.run(
+    "Write a polite reply to an email",
+    context=dict(email=emails[0])
+)
+
+print(reply)
+
+
+## learnings
+## set the default agent
+## doesnt appear to log to prefect, if thats the case, no need for block, just define
+## could be used in a function, potentially, as if it were an endpoint that could be engaged via something like slack/streamlit
 
 
 
