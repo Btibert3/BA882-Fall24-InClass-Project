20cffaa1db30f8076af2fc39d62dd0e37e6ec248
Brock <btibert@bu.edu>
2024-11-22 05:28:53 +0000

    reorg, flow 1 working for cloud flow into prefect cloud

---
+++ genai/controlfow/flow1.py
@@ -0,0 +1,27 @@
+# an agent that fires on an event to prefect -- summarizes a public url
+
+import controlflow as cf
+
+import vertexai
+from vertexai.generative_models import GenerativeModel
+from google.oauth2.service_account import Credentials
+
+from prefect_gcp import GcpCredentials
+gcp_credentials_block = GcpCredentials.load("ba882-fall24-etl-sa")
+
+service_account_json_str = gcp_credentials_block.service_account_info.get_secret_value()
+credentials = Credentials.from_service_account_info(service_account_json_str)
+
+from langchain_google_vertexai import ChatVertexAI
+model = ChatVertexAI(model="gemini-1.5-flash", credentials=credentials)
+
+cf.defaults.model = model
+
+@cf.flow
+def my_flow(x=5):
+    y = cf.run('Add 5 to the number', result_type=int, context=dict(x=x))
+    z = cf.run('Multiply the result by 2', result_type=int)
+    return z
+
+print(my_flow(10))
+
